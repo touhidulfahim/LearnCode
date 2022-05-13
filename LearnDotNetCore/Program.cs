6341ac3362ea1a661e422bffda3094b16527a646
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2022-05-13 22:17:51 +0600

    Project Configuration, Serilog Configuration

---
+++ LearnDotNetCore/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
+
+namespace LearnDotNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configBuilder = new
+                    ConfigurationBuilder().AddJsonFile("appsettings.json", false)
+                .AddEnvironmentVariables()
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft",
+                    LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .ReadFrom.Configuration(configBuilder)
+                .CreateLogger();
+            try
+            {
+                Log.Information("Application Starting up");
+                CreateHostBuilder(args).Build().Run();
+                Log.Information("Application Started Successfully");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80");
+                });
+    }
+}
